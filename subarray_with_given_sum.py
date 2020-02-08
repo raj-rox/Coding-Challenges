e53d13ebee756b238980667d7a0ba93bed0c22ab
RAJ <31188261+raj-rox@users.noreply.github.com>
2020-02-08 21:34:23 +0530

    Add files via upload

---
+++ subarray_with_given_sum.py
@@ -0,0 +1,11 @@
+a=(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+S=15
+for i in range(0,5):
+	sum=a[i]
+	j=i+1
+	while((sum < S+1) and (j<len(a))):
+		sum=sum+a[j]
+		if(sum==S):
+			print(i+1, end=" ")
+			print(j+1)
+		j+=1
