99f1ef789f1d6667db3235e59eaa9f3a0d91c617
RAJ <rajsanchi@gmail.com>
2020-07-18 04:51:18 +0530

    Create division.py

---
+++ division.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
+
+
+https://www.hackerrank.com/challenges/python-division/submissions/code/165814422
