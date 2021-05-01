99b5d2c8eea702973a3a824e5d3f66cc869c9c30
RAJ <rajsanchi@gmail.com>
2021-05-01 14:35:09 +0530

    Add files via upload

---
+++ llprint.py
@@ -0,0 +1,48 @@
+#https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list/problem
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+# Complete the printLinkedList function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def printLinkedList(head):
+    current=head
+    if current:
+        while current:
+            print(current.data)
+            current=current.next
+        
+if __name__ == '__main__':
